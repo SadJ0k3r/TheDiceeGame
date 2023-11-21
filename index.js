6d2356ad5be1120a00cf48407248de450054be8f
Angel Rivera Nieves <50711051+SadJ0k3r@users.noreply.github.com>
2023-11-21 14:38:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+// Wait for the DOM to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function() {
   // Generate a random number between 1 and 6 for Player 1
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
   // Create the filename for the corresponding dice image for Player 1
-  var randomDiceImage = "/dice6" + randomNumber1 + ".png";
+  var randomDiceImage = "/dice" + randomNumber1 + ".png";
 
   // Construct the complete path for the image source for Player 1
-  var randomImageSource = "/dice6" + randomDiceImage;
+  var randomImageSource = randomDiceImage;
 
   // Select the first image element
   var image1 = document.querySelectorAll("img")[0];
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function() {
   if (image1) {
     image1.setAttribute("src", randomImageSource);
   } else {
+    // Log an error message if the image element for Player 1 is not found
     console.error("Image element for Player 1 not found!");
   }
 
@@ -22,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
   // Create the filename for the corresponding dice image for Player 2
-  var randomImageSource2 = "/dice6" + randomNumber2 + ".png";
+  var randomImageSource2 = "/dice" + randomNumber2 + ".png";
 
   // Select the second image element
   var image2 = document.querySelectorAll("img")[1];
@@ -31,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
   if (image2) {
     image2.setAttribute("src", randomImageSource2);
   } else {
+    // Log an error message if the image element for Player 2 is not found
     console.error("Image element for Player 2 not found!");
   }
 
